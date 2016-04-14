9905a6f27e08e5bde574a137c0530db5d1439147
Sanket Kalaskar <sanketka@cybage.com>
2016-04-14 18:39:47 +0530

    Adding basic functionality to People with similar diseases

--- HealthCare.js
+++ HealthCare.js
@@ -1,8 +1,11 @@
+localStorage.setItem('data', '[{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},	{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"},{"name": "bill", "diseases":  "Diabetes", "age": " 45"}]')
 var HelathCare = function() {
   var self= this;
   self.prefix = ko.observableArray(['Mr.','Mrs']);
   self.FirstName = ko.observable();
   self.LastName = ko.observable();
+  self.showPeople = ko.observable(false);
+  self.retrievedData = ko.observableArray([]);
   self.FullName = ko.computed(function() {
     return self.FirstName() + " " + self.LastName();
   },self);
@@ -17,6 +20,19 @@ var HelathCare = function() {
   }
   self.removeMember = function() {
     self.members.remove(this);
+  }
+  $( "#dialog-form" ).dialog({
+    autoOpen: false,
+     modal: true
+  });
+  self.selfEvaluation = function() {
+     $( "#dialog-form" ).dialog( "open" );
+     console.log("hello");
+  }
+  self.showSimilarPeople = function(e) {
+    self.showPeople(true);
+    self.retrievedData( ko.utils.parseJson(localStorage.getItem('data')));
+    console.log(self.retrievedData());
   }
 }
 ko.applyBindings(new HelathCare());
