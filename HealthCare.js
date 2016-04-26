a11e3b020246c1f67765a952c86725ae1e5ad07f
Sanket Kalaskar <sanketka@cybage.com>
2016-04-26 12:23:22 +0530

    Adding additional functionality as study of subscribe and custom bindings

--- HealthCare.js
+++ HealthCare.js
@@ -2,8 +2,14 @@ localStorage.setItem('data', '[{"name": "bill", "diseases":  "Diabetes", "age": 
 var HelathCare = function() {
   var self= this;
   self.prefix = ko.observableArray(['Mr.','Mrs']);
+  self.selectedPatient = ko.observable();
+  self.toDie = ko.observable(false);
+  self.saved = ko.observable(false);
+  self.healthy = ko.observable(false);
+  self.radioSelectedOptionValue = ko.observable(true);
   self.FirstName = ko.observable();
   self.LastName = ko.observable();
+  self.fetchedData = ko.observable();
   self.showPeople = ko.observable(false);
   self.retrievedData = ko.observableArray([]);
   self.FullName = ko.computed(function() {
@@ -34,5 +40,51 @@ var HelathCare = function() {
     self.retrievedData( ko.utils.parseJson(localStorage.getItem('data')));
     console.log(self.retrievedData());
   }
+  self.radioSelectedOptionValue = ko.computed({
+    read: function() { },
+    write: function(newValue) {
+    self.toDie(newValue === "Die" ? true : false);
+    self.saved(newValue === "canBeSaved" ? true : false);
+    self.healthy(newValue === "healthy" ? true : false);
+    }
+  });
 }
-ko.applyBindings(new HelathCare());
+
+ko.bindingHandlers.highlight = {
+    update: function(element, valueAccessor, allBindings) {
+        // First get the latest data that we're bound to
+        var value = valueAccessor();
+        // Next, whether or not the supplied model property is observable, get its current value
+        var valueUnwrapped = ko.unwrap(value);
+
+        // Now manipulate the DOM element
+        if (valueUnwrapped == true)
+           $(element).slideDown(5000); // Make the element fade
+    }
+};
+var HelathCare = new HelathCare();
+ko.applyBindings(HelathCare);
+
+HelathCare.toDie.subscribe(function () {
+   $(document).ready(function(){
+    $("#Die").show();
+    $("#healthy").hide();
+    $("#saved").hide();
+  });
+});
+
+HelathCare.saved.subscribe(function () {
+   $(document).ready(function(){
+    $("#saved").show();
+    $("#Die").hide();
+    $("#healthy").hide();
+  });
+});
+
+HelathCare.healthy.subscribe(function () {
+   $(document).ready(function(){
+    $("#healthy").show();
+    $("#Die").hide();
+    $("#saved").hide();
+  });
+});
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BMICalculator
+
+This project is a example of BMI Calculator implemenation usin KnockoutJS. During the course of my learning KnockoutJS,
+I am implememting the concepts.
